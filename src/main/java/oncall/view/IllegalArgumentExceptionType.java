fe1612bd94c96de4cbb8a7d0b9535ef909695ece
DongHoon Lee <dhl1924@naver.com>
2023-12-16 15:01:07 +0900

    fix(IllegalArgumentExceptionType): 상수 뒤 세미콜론 추가

--- src/main/java/oncall/view/IllegalArgumentExceptionType.java
+++ src/main/java/oncall/view/IllegalArgumentExceptionType.java
@@ -1,7 +1,7 @@
 package oncall.view;
 
 public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
-    NON_VALID_VALUE("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+    NON_VALID_VALUE("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
     private final String message;
 
     IllegalArgumentExceptionType(String message) {

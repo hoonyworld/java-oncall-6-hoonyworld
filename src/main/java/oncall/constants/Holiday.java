eb05bea07204b626f88e1995456cda0724b3add1
DongHoon Lee <dhl1924@naver.com>
2023-12-16 16:58:51 +0900

    fix(Holiday): value 수정

--- src/main/java/oncall/constants/Holiday.java
+++ src/main/java/oncall/constants/Holiday.java
@@ -1,22 +1,21 @@
 package oncall.constants;
 
 public enum Holiday {
-    NEW_YEAR("일", 1, 1),
-    INDEPENDENCE_MOVEMENT_DAY("수",3, 1),
-    CHILDREN_DAY("금",5, 5),
-    MEMORIAL_DAY("화",6, 6),
-    INDEPENDENCE_DAY("화",8, 15),
-    NATIONAL_FOUNDATION_DAY("화",10, 3),
-    HANGUL_DAY("월",10, 9),
-    CHRISTMAS("월", 12, 25);
+    NEW_YEAR("1,일",1),
+    INDEPENDENCE_MOVEMENT_DAY("3,수", 1),
+    CHILDREN_DAY("5,금",  5),
+    MEMORIAL_DAY("6,화",  6),
+    INDEPENDENCE_DAY("8,화",  15),
+    NATIONAL_FOUNDATION_DAY("10,화",  3),
+    HANGUL_DAY("10,월",  9),
+    CHRISTMAS("12,월",  25);
+
 
     private final String DayofTheWeekForHoliday;
-    private final int month;
     private final int day;
 
-    Holiday(String holiday, int month, int day) {
+    Holiday(String holiday, int day) {
         this.DayofTheWeekForHoliday = holiday;
-        this.month = month;
         this.day = day;
     }
 
@@ -26,7 +25,9 @@ public enum Holiday {
 
     public static boolean isHoliday(int month, int day) {
         for (Holiday holiday : Holiday.values()) {
-            if (holiday.month == month && holiday.day == day) {
+            String[] monthAndDayOfWeek = holiday.getHolidayDayOfWeek().split(",");
+            int holidayMonth = Integer.parseInt(monthAndDayOfWeek[0]);
+            if (holidayMonth == month && holiday.day == day) {
                 return true;
             }
         }

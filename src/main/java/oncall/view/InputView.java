dec3daf39aeff052db6343a4bab3aac8515469f4
DongHoon Lee <dhl1924@naver.com>
2023-12-16 14:58:38 +0900

    feat(InputView): 입력 값에 대한 예외처리 구현 완료

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,6 +1,9 @@
 package oncall.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import oncall.domain.MonthAndDay;
 
 public class InputView {
@@ -15,9 +18,14 @@ public class InputView {
             if (input.length != 2) {
                 throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
             }
+            for (String s : input) {
+                if (s.trim().isEmpty()) {
+                    throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
+                }
+            }
 
-            int month = Integer.parseInt(input[0]);
-            String startDay = input[1];
+            int month = Integer.parseInt(input[0].trim());
+            String startDay = input[1].trim();
 
             if (month < 1 || month > 12) {
                 throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
@@ -27,11 +35,12 @@ public class InputView {
             }
 
             return new MonthAndDay(month, startDay);
-        } catch (NumberFormatException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(IllegalArgumentExceptionType.NON_VALID_VALUE.getMessage());
             return readMonthAndDay();
         }
     }
+
 
     private static boolean isValidStartDay(String day) {
         String[] validDays = {"일", "월", "화", "수", "목", "금", "토"};
@@ -44,29 +53,57 @@ public class InputView {
     }
 
     public static String[] readWeekdayOncallOrder() {
-        return readOncallOrder("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
-    }
-
-    public static String[] readHolidayOncallOrder() {
-        return readOncallOrder("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
-    }
-
-    private static String[] readOncallOrder(String prompt) {
         try {
-            System.out.print(prompt);
+            System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
             String[] oncallOrder = Console.readLine().split(",");
-            validateOncallOrder(oncallOrder);
+            validateOncallOrder(oncallOrder, false); // 평일 근무 순번에 대한 검증
             return oncallOrder;
         } catch (IllegalArgumentException e) {
             System.out.println(IllegalArgumentExceptionType.NON_VALID_VALUE.getMessage());
-            return readOncallOrder(prompt);
+            return readWeekdayOncallOrder();
         }
     }
 
-    private static void validateOncallOrder(String[] oncallOrder) {
-        if (oncallOrder.length == 0 || hasEmptyName(oncallOrder)) {
+    public static String[] readHolidayOncallOrder() {
+        try {
+            System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+            String[] oncallOrder = Console.readLine().split(",");
+            validateOncallOrder(oncallOrder, true); // 휴일 근무 순번에 대한 검증
+            return oncallOrder;
+        } catch (IllegalArgumentException e) {
+            System.out.println(IllegalArgumentExceptionType.NON_VALID_VALUE.getMessage());
+            return readHolidayOncallOrder();
+        }
+    }
+
+
+    private static void validateOncallOrder(String[] oncallOrder, boolean isHoliday) {
+        if (oncallOrder.length == 0 || hasEmptyName(oncallOrder) || hasDuplicate(oncallOrder)) {
             throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
         }
+        if (!hasValidNicknameLength(oncallOrder)) {
+            throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
+        }
+        if (oncallOrder.length < 5) {
+            throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
+        }
+        if (isHoliday && oncallOrder.length > 35) {
+            throw IllegalArgumentExceptionType.NON_VALID_VALUE.getException();
+        }
+    }
+
+    private static boolean hasValidNicknameLength(String[] names) {
+        for (String name : names) {
+            if (name.length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+        private static boolean hasDuplicate(String[] names) {
+        Set<String> nameSet = new HashSet<>(Arrays.asList(names));
+        return nameSet.size() != names.length;
     }
 
     private static boolean hasEmptyName(String[] names) {

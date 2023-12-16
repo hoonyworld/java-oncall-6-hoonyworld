1520717296286e7b16bfa31278d720f463ff097c
DongHoon Lee <dhl1924@naver.com>
2023-12-16 14:30:40 +0900

    feat(IllegalArgumentExceptionType): ExceptionType을 override하는 enum 생성

---
+++ src/main/java/oncall/view/IllegalArgumentExceptionType.java
@@ -0,0 +1,20 @@
+package oncall.view;
+
+public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
+    NON_VALID_VALUE("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+    private final String message;
+
+    IllegalArgumentExceptionType(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public IllegalArgumentException getException() {
+        return new IllegalArgumentException(getMessage());
+    }
+}
